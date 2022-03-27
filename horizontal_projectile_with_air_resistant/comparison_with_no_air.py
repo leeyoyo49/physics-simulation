00e089c47ff037998664d0141531e8c52ddb8289
H.Y Lee <lee.s.yoyo0409@gmail.com>
2022-03-27 22:33:10 +0800

    bug fix

--- horizontal_projectile_with_air_resistant/comparison_with_no_air.py
+++ horizontal_projectile_with_air_resistant/comparison_with_no_air.py
@@ -3,7 +3,7 @@ from vpython import *
 
 size = 1
 m = 5
-v = 5
+v = 15
 L = 50
 g = -9.8
 dt = 0.0001
@@ -13,13 +13,12 @@ angle = math.asin(4/5) #水平夾角53度
 
 scene = canvas(title="Projectile", width=800, height=600, x=0, y=0, center=vec(0, 0, 0), background=vec(0, 0.6, 0.6))
 floor = box(pos=vec(0, -size, 0), size=vec(L, 0.01, 10), texture=textures.metal)
-ball = sphere(pos=vec(-L/2, 0, 0), radius=size, texture=textures.wood, make_trail=True, v=vec(9, 12, 0), a=vec(0, g, 0))
-ball2 = sphere(pos=vec(-L/2, 0, 0), radius=size, texture=textures.wood, make_trail=True, v=vec(9, 12, 0), a=vec(0, g, 0))
+ball = sphere(pos=vec(-L/2, 0, 0), radius=size, texture=textures.wood, make_trail=True, v=vec((math.cos(angle)*v), (math.sin(angle)*v), 0), a=vec(0, g, 0))
+ball2 = sphere(pos=vec(-L/2, 0, 0), radius=size, texture=textures.wood, make_trail=True, v=vec((math.cos(angle)*v), (math.sin(angle)*v), 0), a=vec(0, g, 0))
 
 while ( ball2.pos.y > 0 ) or ( ball.pos.x==-L/2 ):
-    print(t,"sec passed")
     rate(10000)
-    Fk = -1/2 * k * v * v
+    Fk = -1/2 * k * (ball.v.x*ball.v.x+ball.v.y*ball.v.y)
     if (ball.pos.y>=0):
         ball.a.x = (Fk / m)*math.cos(angle)
         ball.a.y = (Fk / m)*math.sin(angle) + g
@@ -29,4 +28,7 @@ while ( ball2.pos.y > 0 ) or ( ball.pos.x==-L/2 ):
     ball2.v += ball2.a*dt
     ball2.pos += ball2.v*dt
     t+=dt
+
+
+print(ball.v.x,ball.v.y)
 print(f"ball1's deltax:{ball.pos.x+L/2} \nball2's deltax:{ball2.pos.x+L/2}")
--- horizontal_projectile_with_air_resistant/max_length_different_angle.py
+++ horizontal_projectile_with_air_resistant/max_length_different_angle.py
@@ -2,12 +2,12 @@ import math
 from vpython import *
 
 size = 1
-m = 10
-v = 15
-L = 50
+m = 0.0012
+v = 13.9
+L = 10
 g = -9.8
 dt = 0.0001
-k = 1
+k = 0.00077
 t = 0
 max_length = 0
 
@@ -17,17 +17,22 @@ floor = box(pos=vec(0, -size, 0), size=vec(L, 0.01, 10), texture=textures.metal)
 for x in range(90):
     angle = x*(math.pi/180)
     ball = sphere(pos=vec(-L/2, 0, 0), radius=size, texture=textures.wood, make_trail=True, v=vec(math.cos(angle)*v, math.sin(angle)*v, 0), a=vec(0, g, 0))
+    
     while ( ball.pos.y > 0 ) or ( ball.pos.x==-L/2 ):
-        rate(100000)
-        Fk = -1/2 * k * v * v
+        rate(10000)
+        Fk = -1/2 * k * (ball.v.x*ball.v.x+ball.v.y*ball.v.y)
         if (ball.pos.y>=0):
             ball.a.x = (Fk / m)*math.cos(angle)
             ball.a.y = (Fk / m)*math.sin(angle) + g
             ball.v += ball.a*dt
             ball.pos += ball.v*dt
+
     length = ball.pos.x+L/2
+
     if length > max_length :
         max_length = length
+        biggest_angle = angle
+
         print(max_length)
         if x == 0:
             big = ball
@@ -36,11 +41,10 @@ for x in range(90):
         big.clear_trail()
         del big
         big = ball
-    
-    if length < max_length :
+    elif length < max_length :
         ball.visible = False
         ball.clear_trail()
         del ball
         break
 
-print(max_length, angle*180/math.pi)
+print(f"ball's max displacement:{max_length} \nand the angle is {biggest_angle*180/math.pi}" )
